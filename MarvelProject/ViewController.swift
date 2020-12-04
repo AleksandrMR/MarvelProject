d3500704d1e88ecd3572b1c0898eb0a4c95829cd
Aleksandr Milashevski <aleksandr.milashevski.ios@gmail.com>
2020-12-04 12:15:37 +0100

    Initial Commit

---
+++ MarvelProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelProject
+//
+//  Created by Aleksandr Milashevski on 04/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
